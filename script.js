a5b36c381ed46c65bcd2ac58ab090b71875be89a
Adhithyas27 <120027992+Adhithyas27@users.noreply.github.com>
2024-11-02 22:02:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+var number=Math.floor(Math.random()*20)+1
+document.querySelector("#bt1").addEventListener("click",function(){
+    number=Math.floor(Math.random()*20)+1
+    n1=20;
+    document.querySelector("input").value='';
+    document.querySelector("#span1").innerHTML='starting.....'
+    document.querySelector("#span2").innerHTML="20";
+    document.querySelector(".container").classList.remove("gameover")
+    document.querySelector("#bt2").classList.remove("invisible")
+    document.querySelector('#id2').innerHTML='Guess the number';
+    document.querySelector(".container").classList.remove("win")
+    document.querySelector(".qmark").innerHTML="?";
+    
+
+})
+var n1=20;
+
+document.querySelector("#bt2").addEventListener("click",function(){
+    var input=document.querySelector("input").value;
+    if(input>number){
+        document.querySelector("#span1").innerHTML='Too high.';
+        n1--;
+        document.querySelector("#span2").innerHTML=n1;
+        if(n1==0){
+            document.querySelector(".container").classList.add("gameover")
+        }
+     
+
+    }
+    else if(input<number){
+        document.querySelector("#span1").innerHTML='Too low.'
+        n1--;
+        document.querySelector("#span2").innerHTML=n1;
+        
+        if(n1==0){
+            document.querySelector(".container").classList.add("gameover")
+            document.querySelector("#bt2").classList.add("invisible")
+            
+
+            
+        }
+   }
+   else if(input==number){
+    document.querySelector("#span1").innerHTML='Correct value....'
+    document.querySelector('#id2').innerHTML="Number found";
+    document.querySelector(".container").classList.add("win")
+    document.querySelector("#span3").innerHTML=n1;
+    document.querySelector(".qmark").innerHTML=" "+number+" ";
+    
+   }
+})
+
